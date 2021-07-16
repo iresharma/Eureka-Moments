44c6d097ed04b9cbab91bacaa42087fc79a21cd6
bharath <bharathnadig19@gmail.com>
2021-07-16 16:04:40 +0530

    added question9

---
+++ src/views/Slides/slide9-1.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="main">
+        <div class="content">
+            <p>
+                <strong>We know that in Logic Gates</strong>
+            </p>
+            <div class="options">
+            Y= A + B => OR Gate<br />
+            Y= A . B => AND Gate<br />
+            Y=A’ => NOT Gate<br />
+            <image src="@/assets/images/q9_1.jpeg" alt="" />
+            </div>
+        </div>
+        <a-button
+      size="large"
+      icon="right"
+      shape="round"
+      @click="$store.commit('INCREMENT_STATE')"
+      >Next</a-button>
+    </div>
+</template>
+
+<style lang="scrc" scoped>
+.main {
+    padding: 5rem;
+
+}
+</style>
